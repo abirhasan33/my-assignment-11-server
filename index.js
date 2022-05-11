f73321b2904aa7fb0920b2980ba445e81fafb50b
abirhasan33 <abir03512@gmail.com>
2022-05-11 15:54:09 +0600

    delete invintryo add

--- index.js
+++ index.js
@@ -108,13 +108,7 @@ async function run(){
         });
 
 
-        // delete
-        app.delete('/order/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await orderCollection.deleteOne(query);
-            res.send(result);
-        });
+        
 
     }
     finally{
