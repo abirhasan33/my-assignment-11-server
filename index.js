f01f2a3e9bb5b3a780c807858d16774f9e3b0c46
abirhasan33 <abir03512@gmail.com>
2022-05-11 15:53:34 +0600

    delete repo add

--- index.js
+++ index.js
@@ -71,7 +71,13 @@ async function run(){
         });
 
 
-        
+         // DELETE
+         app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        });
 
         // PUT 
         app.put('/service/:id', async(req, res)=> {
